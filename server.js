a612fb577a60e88e3bf2cff3d9e5644744e6c163
qsparks <q.m.sparks@gmail.com>
2021-01-15 13:31:41 -0600

    builds base server structure

---
+++ server.js
@@ -0,0 +1,17 @@
+// ANCHOR external imports
+const express = require('express');
+
+// ANCHOR internal imports
+// const routes = require('./routes');
+
+// ANCHOR config
+// require("dotenv").config();
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// ANCHOR middleware
+
+// ANCHOR routes
+
+// ANCHOR connection
+app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
