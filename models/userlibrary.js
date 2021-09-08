ef405f7b0d8d972e397f85313650e0bfb37808bc
qsparks <q.m.sparks@gmail.com>
2021-09-08 12:21:28 -0500

    adds user library and wishlist models

---
+++ models/userlibrary.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserLibrary extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  UserLibrary.init({
+    UserId: DataTypes.INTEGER,
+    GameId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'UserLibrary',
+  });
+  return UserLibrary;
+};
